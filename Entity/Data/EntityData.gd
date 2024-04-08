b0eee34e4f8d4e83d754fde98556b84480859475
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-04-08 09:37:56 +0800

    Version 0.0.m3.0d
    [ADDED]
    1. Player character
    2. Entity class
    3. Entity Data
    4. Player script
    5. Crystal, Forest, home and Lava levels
    6. Main menu scene
    [CHANGES}
    ~~~~~~~
    [BUG FIXES]
    ~~~~~~~

---
+++ Entity/Data/EntityData.gd
@@ -0,0 +1,8 @@
+class_name EntityData
+extends Resource
+
+@export var Name:String
+@export var MaxHealth:float
+@export var MaxStamina:float
+@export var MaxMana:float
+
---
+++ Entity/Entity.gd
@@ -0,0 +1,13 @@
+class_name Entity
+extends CharacterBody3D
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _moveToPos(pos:Vector3):
+	pass
