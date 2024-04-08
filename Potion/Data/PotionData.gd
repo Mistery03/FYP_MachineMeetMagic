ae16004dfdba470ba299dd1273ba8f8e401c7040
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-04-08 16:50:49 +0800

    ver 0.0.m3.1d
    [ADDED]
    1. Potion Data
    2. Machine Data
    3. Healing Potion
    4. Healing Potion data
    5. Healing potion scene
    [CHANGES}
    ~~~~~~~
    [BUG FIXES]
    ~~~~~~~

---
+++ Potion/Data/PotionData.gd
@@ -0,0 +1,8 @@
+class_name PotionData
+extends Resource
+
+@export var name:String
+@export var category:String
+@export var isThrowable:bool
+@export var potion:PackedScene
+@export var potionSprite:Texture2D
