8388d49efb740eeb0cd4690fb04ae1af84d60515
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-04-11 21:13:50 +0800

    ver 0.0.m3.5d
    [ADDED]
    1)Potion Manager
    2) Input maps:
        - Action:LeftMseBTn
        - DrinkPotion:Q
        - ChangePotion:Ctrl+P
        - UseUtility:U
        - MoveForward:W
        - MoveBackward:S
        - MoveLeft:A
        - MoveRight:D
    [CHANGES]
    1) Changed Camera position and angle
    [BUG FIXES]
    ~~~~~~~
    [NOTE TO SELF]
    1. Find ready made assets that use gltf files
    2. Potions are not done
    3. Machines are not done
    4. Test potion abstraction
    5. Test machine abstraction
    6. Add animation for drinking
    7. Might to need modify the potion class to support animation and yield time

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -6,6 +6,11 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 @export var playerData:EntityData
+@export var potionObject:PotionData
+
+@onready var potion_manager = $PotionManager
+
+var potion:Potion
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -14,6 +19,7 @@ func _ready():
 	currHealth = playerData.MaxHealth
 	currMana = playerData.MaxMana
 	currStamina = playerData.MaxStamina
+	potion_manager.init(self)
 	
 
 func _physics_process(delta):
@@ -37,3 +43,7 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _process(delta):
+	pass
+
--- Potion/Data/PotionData.gd
+++ Potion/Data/PotionData.gd
@@ -3,6 +3,8 @@ extends Resource
 
 @export var name:String
 @export var category:String
+@export var amount:int= 0
 @export var isThrowable:bool
-@export var potion:PackedScene
+@export var scene:PackedScene
 @export var potionTexture:Texture2D
+
