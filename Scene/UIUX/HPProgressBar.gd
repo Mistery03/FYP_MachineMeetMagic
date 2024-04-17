75d8cdf35e5f25bd9f887d88f16b12ab848dba2a
Rinka <76248335+RinkaWai0210@users.noreply.github.com>
2024-04-18 03:50:19 +0800

    ver 0.0.m3c.1
    [ADDED]
    1. HP Progress Bar Down and Progress Image
    2. HP and Mana Progress Bar Script
    [CHANGES}
    1.
    [BUG FIXES]
    1. If we want to put the HUD or whatever mechanic into the player then it has to be within the player class/scene
    2. If we want to put the HUD or whatever mechanic into the scenes/levels then it has to be within the scenes/levels
    [NOTE TO SELF]
    1. Change Mana Progress Color
    2. Find Bag asset

--- Scene/UIUX/HPProgressBar.gd
+++ Scene/UIUX/HPProgressBar.gd
@@ -1,14 +1,20 @@
 extends TextureProgressBar
 
-@export var player: Player
-
+var player: Player
+signal increaseHP
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	update() # Replace with function body.
+	#value = player.currHealth
+	pass
+	#update() # Replace with function body.
 
-func update():
-	value = player.currHealth * 100 / 100
+func update(player: Player):
+	value = player.currHealth
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func increaseHealth():
+	
