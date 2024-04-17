cf839178eabef1088aef0a37fee72fba23f58f4e
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-04-17 20:03:23 +0800

    VERSION OVERHAUL TO 2D
    Changing HD2D to pure 2D

--- Machine/Machine.gd
+++ Machine/Machine.gd
@@ -4,6 +4,7 @@ extends Node
 var machine:Machine 
 var next:Machine = null
 var ray:RayCast3D # Called when the node enters the scene tree for the first time.
+var pos:Vector3
 
 
 
@@ -13,14 +14,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var viewport := get_viewport()
-	var mouse_position := viewport.get_mouse_position()
-	var camera := viewport.get_camera_3d()
-	var origin := camera.project_ray_origin(mouse_position)
-	var direction := camera.project_ray_normal(mouse_position)
-	
-	ray.target_position =camera.project_local_ray_normal(mouse_position)
+
 
 	
+	
 	print("test")
+
 		
--- Machine/testMachine.gd
+++ Machine/testMachine.gd
@@ -5,8 +5,8 @@ extends Machine
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ray = ray_cast_3d
+	pos = self.position 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	super(delta)
+func _process(delta):	super(delta)
