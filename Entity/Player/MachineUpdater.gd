b19998e884ef4d83d0051d917f6439d83f8d37fb
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-05-16 15:40:50 +0800

    Bug fixes

--- Entity/Player/MachineUpdater.gd
+++ Entity/Player/MachineUpdater.gd
@@ -10,14 +10,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	for battery in batteryList:
 		for machine in machineList:
-			if battery.percentage > 0:
+			if battery.percentage > 0 :
 				machine.isThereFuel = true
 				if machine.machineUI.power_switch.button_pressed:
 					battery.consumeMana(machine.manaConsumptionPerSecond,delta)	
+				elif machine.percentage < 100:
+					battery.consumeMana(machine.manaConsumptionPerSecond,delta)
+					
 			else:
-				machine.isThereFuel = false	
+				if machine != null:
+					machine.isThereFuel = false	
 				
 func setMachineUpdaterData(batteryList:Array,machineList:Array):
 	self.batteryList = batteryList
