fabbdfc92b3e18b08eb6073fe80e9ef4672d4620
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-05-27 03:08:29 +0800

    Checkpoint 9

--- Scene/Forest/Forest.gd
+++ Scene/Forest/Forest.gd
@@ -8,16 +8,19 @@ var roomList:Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
 	create_spanning_tree(maxNodes)
 	print_adjacency_list()
 	roomList.clear()
 	for node in getAdjacencyDegreeList():
 		var room = DegreeRoom.get(getAdjacencyDegree(int(node))).instantiate()
 		room.visible = false
+		room.position = Vector2(1600,1600)
 		if player:
 			room.player = player
 		roomList.append(room)
 		rooms.add_child(room)
-		
+	
+	roomList[0].position = Vector2.ZERO	
 	roomList[0].visible = true
 	roomList[0].isStartingRoom = true
