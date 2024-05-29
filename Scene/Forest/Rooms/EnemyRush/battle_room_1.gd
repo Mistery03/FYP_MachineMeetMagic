f35362ceeb7cae8d1894ec31e96bac27011e5a28
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-05-29 23:45:49 +0800

    Checkpoint 24

---
+++ Scene/Forest/Rooms/EnemyRush/battle_room_1.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@export_category("Room Settings")
+@export var PK_roomID:int = 128
+@export var roomID:int
+@export var roomNum:int = 0
+@export_category("Player")
+@export var player:Player
