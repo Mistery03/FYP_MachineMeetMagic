3a0cfba46a47ba48c7d309e0af635a94a9b86211
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-05-29 23:28:43 +0800

    Checkpoint 23

--- Scene/Forest/Rooms/Starting/StartingArea.gd
+++ Scene/Forest/Rooms/Starting/StartingArea.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@export_category("Room Settings")
+@export var PK_roomID:int = 128
+@export var roomID:int
+@export var isStartingRoom:bool
+@export var roomNum:int = 0
+@export_category("Player")
+@export var player:Player
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
