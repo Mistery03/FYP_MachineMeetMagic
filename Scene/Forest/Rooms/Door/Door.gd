a02292728dfaab5e707cdbc653793e5678a613fd
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-05-29 22:48:25 +0800

    Cehcekpoint 22

--- Scene/Forest/Rooms/Door/Door.gd
+++ Scene/Forest/Rooms/Door/Door.gd
@@ -3,16 +3,14 @@ extends Node2D
 
 @export var roomID:int = 0
 @export var roomNumber:int = 0
-@export var prevRoomNumber:int = 0
-@export_enum("UP","RIGHT","LEFT","DOWN") var orientation:String
 @export var fadeOut:TextureRect
 @export var flag:Sprite2D
 
-signal OnDoorEntered(roomNumber,roomID,prevRoomNumber)
+signal OnDoorEntered(roomNumber,roomID)
 
 func _on_area_2d_body_entered(body):
 	if fadeOut:
 		var tween = get_tree().create_tween()
 		tween.tween_property(fadeOut,"modulate:a",1,0.5)
-	OnDoorEntered.emit(roomNumber,roomID,prevRoomNumber)
+	OnDoorEntered.emit(roomNumber,roomID)
 	
