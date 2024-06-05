5155e775f01cfc8c0c7ed2e8b1d2208a844a8a31
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-05 19:16:08 +0800

    Checkpoint 42, refactoring needed

--- Entity/Player/Action.gd
+++ Entity/Player/Action.gd
@@ -4,7 +4,8 @@ extends State
 var move_state: State
 @export
 var build_state:State
-
+@export 
+var action_state:State
 
 @export
 var inputList:Dictionary= {
@@ -17,23 +18,11 @@ var isBuildEnabled:bool = true
 
 func enter() -> void:
 	super()
-	camera.position_smoothing_enabled = false
 	
-	lerp_to_zero()
-	parent.itemHUDPlaceholder.visible = true
-	await parent.get_tree().create_timer(0.2).timeout
-	parent.isPressable = true
-	parent.velocity.x = 0.0
-	
-	
-func lerp_to_zero():
-	# Gradually lerp the velocity to 0
-	parent.velocity.x = lerp(parent.velocity.x, 0.0, 0.2)
-	parent.velocity.y = lerp(parent.velocity.y, 0.0, 0.2)
 
-# Check if the velocity is close enough to 0
-	if abs(parent.velocity.x) < 0.01:
-		parent.velocity.x = 0.0
+	
+	
+
 
 func process_input(event: InputEvent) -> State:
 	move_component.axis = move_component.get_movement_direction()
@@ -48,21 +37,17 @@ func process_input(event: InputEvent) -> State:
 		parent.isPressable = false
 		return build_state
 	
-	if Input.is_action_just_pressed("EXIT") and parent.isPressable:
-		toggle_menu()
+	
 	return null
 
 func process_frame(delta: float) -> State:
 	isBuildEnabled = parent.isBuildEnabled
 	if parent.staff:
 		if parent.isStaffEquipped:
-			#parent.staff.customAnimation.play("idleFront")
-			pass
+			parent.staff.customAnimation.play("idleFront")
 		else:
 			parent.staff.customAnimation.play("RESET")
 	
 	return null
 
-func toggle_menu():
-	# Toggle the visibility of the menu
-	parent.playerInventoryController.visible = !parent.playerInventoryController.visible 
+
--- Scene/Forest/Rooms/Starting/StartingArea.gd
+++ Scene/Forest/Rooms/Starting/StartingArea.gd
@@ -1,14 +1,20 @@
 extends Room
 
 @export var materialInstance:PackedScene 
-@export var max_drop_items: int = 5  
-
+@export var max_drop_items: int = 5 
+@export var colourAdjustment:Color
+const HIGHLIGHT_SHADER = preload("res://Shaders/highlightShader.material")
 var forest = FastNoiseLite.new()
 var fixedSeed: int = randi()
 var treeSelection:Array[Vector2i] = [Vector2i(0,0),Vector2i(3,0)]	
 var grassData:TileData
 var isClicked:bool = false
 var is_interacting:bool = false 
+var is_holding = false
+var hold_time = 0.0
+var required_hold_time = 2.0
+var materialShader:ShaderMaterial
+var prevMouseTilePos = Vector2i(-1000,-1000)
 
 
 func _ready():
@@ -20,27 +26,38 @@ func _ready():
 			spawnforest(coords.x,coords.y)
 	
 func _process(delta):
+
 	if player:
 		var mouseTilePos = tile_map.local_to_map(player.mousePos)
 		var parentPos = tile_map.local_to_map(player.position)
 		var materialDroppedData = tile_map.get_cell_tile_data(4,mouseTilePos)
 		var validLocation = tile_map.get_surrounding_cells(mouseTilePos)
+		if materialDroppedData:
+			tile_map.set_cell(5,mouseTilePos,2,tile_map.get_cell_atlas_coords(4,mouseTilePos))
+			tile_map.set_layer_modulate(5,Color.SKY_BLUE)
+		if mouseTilePos != prevMouseTilePos:
+			tile_map.erase_cell(5,prevMouseTilePos)
+		prevMouseTilePos = mouseTilePos
+		
 		for validPos in validLocation:
 			if parentPos == validPos:
-				if Input.is_action_just_pressed("ACTION") and player.isStaffEquipped and !is_interacting:
-					
-					#player.staff.customAnimation.queue("CUTTING")
+				if Input.is_action_pressed("ACTION") and player.isStaffEquipped and !is_interacting:
 					player.staff.global_position = player.mousePos
 					if materialDroppedData:
+						player.staff.customAnimation.play("CUTTING")
 						is_interacting = true
 						#player.staff.customAnimation.stop()
-						player.staff.customAnimation.play("CUTTING")
 						await player.staff.customAnimation.animation_finished
+						tile_map.erase_cell(5,prevMouseTilePos)
 						dropMaterials(materialDroppedData,mouseTilePos)
 						is_interacting = false
-					else:
-						is_interacting = false
+						player.staff.customAnimation.play("idleFront")
 					
+			
+					
+		
+				
+		
 					
 
 func spawnforest(x, y):
