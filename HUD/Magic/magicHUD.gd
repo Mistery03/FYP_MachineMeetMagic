862336aaa35bed97ab419e995bb2a9f39da77eb3
Rinka <76248335+RinkaWai0210@users.noreply.github.com>
2024-06-26 00:39:03 +0800

    Magic.6

--- HUD/Magic/magicHUD.gd
+++ HUD/Magic/magicHUD.gd
@@ -4,9 +4,10 @@ extends Control
 
 var currIndex:int = 0
 var prevIndex:int = 0
-var currIncreament:int = 0
-var isMagicnull:bool = false
-var unlocked_magic 
+var currIncrement:int = 0
+var isMagicNull:bool = false
+var unlocked_magic: Array = []
+var magicSize:Array = [null, null, null, null, null]
 @onready var texture_rect = $border/TextureRect
 @onready var player = $"../../.."
 @onready var darkened = $Darkened
@@ -19,46 +20,74 @@ func _ready():
 func _process(delta):
 	pass
 
+func storeArray():
+	var unlock_indices = magicManager.checkMagicUnlock()
+	unlocked_magic.clear()  # Clear the current dynamic array
+	
+	for i in len(unlock_indices):
+		#print("magicDataList:", magicManager.magicDataList[index])
+		unlocked_magic.append(magicManager.magicDataList[i])
+		
+	transfer_to_fixed_array()
+
+func transfer_to_fixed_array():
+	# Clear the fixed array first
+	for i in range(magicSize.size()):
+		magicSize[i] = null
+	# Transfer items from unlocked_magic to magicSize
+	for i in range(min(unlocked_magic.size(), magicSize.size())):
+		magicSize[i] = unlocked_magic[i]
+		print("array", magicSize[i])
+	
 func _unhandled_key_input(event):
 	
 	if Input.is_action_just_pressed("EQUIPMAGIC"):
-		unlocked_magic = magicManager.checkMagicUnlock()
-		print("size",unlocked_magic.size())
-		if unlocked_magic.size() > 0:
+		storeArray()
+		print("Fixed array size: ", magicSize.size())
+		print("Fixed array content: ", magicSize)
+
+		if magicSize.size() > 0:
 			prevIndex = currIndex
-			
-			print("Incre ",currIncreament)
-			currIndex = currIncreament % unlocked_magic.size()
-			print("Index ",currIndex)
-			currIncreament += 1
-			print("Logic",currIncreament >= unlocked_magic.size())
-			print("Logic2",currIndex != prevIndex)
+			print("Incre ", currIncrement)
+
+			currIndex = currIncrement % magicSize.size()
+			print("Index ", currIndex)
+
+			currIncrement += 1
+			print("Logic", currIncrement >= magicSize.size())
+			print("Logic2", currIndex != prevIndex)
 			print('preindex', prevIndex)
-			if currIncreament >= unlocked_magic.size() and currIndex != prevIndex:
-				currIncreament = 0
-			
-			if unlocked_magic[currIndex]:
+
+			if currIncrement >= magicSize.size():
+				currIncrement = 0
+
+			var current_magic = magicSize[currIndex]
+			if current_magic:
 				player.isMagicAvailable = true
-				if unlocked_magic[currIndex].attackBased:
+
+				if current_magic.attackBased:
 					texture_rect.visible = true
-					texture_rect.texture = unlocked_magic[currIndex].skillButton
-					player.magic_manager.magicData = unlocked_magic[currIndex]
+					texture_rect.texture = current_magic.skillButton
+					player.magic_manager.magicData = current_magic
 				else:
 					print("Magic not attack based")
 			else:
-				player.isMagicAvailable =  false
+				player.isMagicAvailable = false
 				texture_rect.visible = false
-				player.magicManager.magicData = null
-				isMagicnull = true
-			
-	if isMagicnull:
-		print("magic", isMagicnull)
-		currIncreament += 1
+				player.magic_manager.magicData = null
+				isMagicNull = true
+
+	if isMagicNull:
+		print("magic", isMagicNull)
+		currIncrement += 1
 		prevIndex = currIndex
-		currIndex = currIncreament % unlocked_magic.size()
-		
-		currIncreament = clamp(currIncreament,0,unlocked_magic.size())
-		if currIncreament == unlocked_magic.size() and currIndex != prevIndex:
-			currIncreament = 0
-		if unlocked_magic[currIndex]:
-			isMagicnull = false
+		currIndex = currIncrement % magicSize.size()
+
+		currIncrement = clamp(currIncrement, 0, magicSize.size())
+
+		if currIncrement == magicSize.size():
+			currIncrement = 0
+
+		var next_magic = magicSize[currIndex]
+		if next_magic:
+			isMagicNull = false
