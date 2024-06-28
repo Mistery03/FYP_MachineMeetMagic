921c84c37e128e1a3077cdc1cf0c915572cea456
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-28 19:42:30 +0800

    Update Entity.gd

--- Entity/Entity.gd
+++ Entity/Entity.gd
@@ -20,7 +20,6 @@ var currHealth:float = 0
 var currStamina:float = 0
 var currMana:float = 0
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
