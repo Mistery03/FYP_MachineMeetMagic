9aec08c76bbddc1e3bdcfea935d0df77495511b1
Rinka <76248335+RinkaWai0210@users.noreply.github.com>
2024-06-29 04:17:46 +0800

    research point updated

--- UI/CharacterScene/character_ui.gd
+++ UI/CharacterScene/character_ui.gd
@@ -3,18 +3,30 @@ class_name researchTree extends Control
 @onready var machine_tree = $machineTree
 @onready var skill_tree = $skillTree
 @onready var description_box = $"Description box"
+@onready var currency = $CurrencyHolder/Currency
+@export var player:Player
 
 var skill_name:String
 var skill_description:String
 var magicData: MagicData
+var researchCurrency:int
 
 var machine_name:String
 var machine_description:String
 var buildingData:BuildingData
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if PlayerGlobal.playerResearchPoint >0:
+		researchCurrency = PlayerGlobal.playerResearchPoint
+		
+	else:
+		researchCurrency = player.ResearchPointCurrency
+	currency.text = str(researchCurrency)
+	print("currency:", researchCurrency)
 	
-	pass # Replace with function body.
+	 # Replace with function body.
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		pass
+	researchCurrency = player.ResearchPointCurrency
+	currency.text = str(researchCurrency)
+	print("currency:", researchCurrency)
