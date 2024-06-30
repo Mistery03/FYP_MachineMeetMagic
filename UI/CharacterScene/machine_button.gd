b98ed751a4aa39bfc16249f5d3b81ce575f4b855
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-30 11:52:49 +0800

    Wai changes

--- UI/CharacterScene/machine_button.gd
+++ UI/CharacterScene/machine_button.gd
@@ -29,6 +29,7 @@ func _ready():
 	if get_parent() is machineButton:
 		line_2d.add_point(global_position + size/2)
 		line_2d.add_point(get_parent().global_position + size/2)
+		
 	if PlayerGlobal.playerResearchPoint >0:
 		researchCurrency = PlayerGlobal.playerResearchPoint
 		
